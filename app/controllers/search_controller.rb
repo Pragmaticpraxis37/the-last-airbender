9745744ada50d38a40f1241d2bf7c9d704728699
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-05 09:56:43 -0600

    Begin creating ostruct object

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,21 @@
+require 'ostruct'
+
 class SearchController < ApplicationController
 
   def index
     formatted_query_term = params[:nation].gsub(/_/, '+')
-    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{formatted_query_term}")
+    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=150&affiliation=#{formatted_query_term}")
     total_data = JSON.parse(response.body, symbolize_names: true)
 
-    total_data.each do |data|
+    total_number_of_people = total_data.length
+
+    total_data.reduce([]) do |acc, data|
+      # acc << OpenStruct.new({
+      #                         name: data[:name],
+      #                         allies: data[:allies],
+      #                         enemies: data[:enemies],
+      #                         affiliations: data[:affiliation]
+      #   })
       require "pry"; binding.pry
     end
   end
