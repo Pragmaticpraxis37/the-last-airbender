5157c14538392303f0b8149d2255023a6cbf5a6f
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-05 09:37:04 -0600

    Review data in SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,13 @@
 class SearchController < ApplicationController
 
   def index
-    require "pry"; binding.pry
+    formatted_query_term = params[:nation].gsub(/_/, '+')
+    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{formatted_query_term}")
+    total_data = JSON.parse(response.body, symbolize_names: true)
 
-    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=#{params[:nation]}")
-    json = JSON.parse(response.body, symbolize_names: true)
-
-
+    total_data.each do |data|
+      require "pry"; binding.pry
+    end
   end
 
 end
